e13cc8ff4a87d213474ac312f6285377967d0b10
chris7395328 <123130884@.com>
2026-03-13 02:29:50 +0800

    Fix code style in searchMangaRequest

--- src/ja/rawlazy/src/eu/kanade/tachiyomi/extension/ja/rawlazy/RawLazy.kt
+++ src/ja/rawlazy/src/eu/kanade/tachiyomi/extension/ja/rawlazy/RawLazy.kt
@@ -94,9 +94,8 @@ class RawLazy : HttpSource() {
 
     // 创建搜索漫画的请求。
     // 使用查询参数 's_manga'
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        return GET("$baseUrl/?s_manga=$query", headers)
-    }
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request =
+        GET("$baseUrl/?s_manga=$query", headers)
 
     // 解析搜索结果与解析热门列表相同
     override fun searchMangaParse(response: Response): MangasPage = popularMangaParse(response)
